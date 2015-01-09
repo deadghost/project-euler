db297ce162fae852b05ea4f6a069a7fd581d4d87
Kenny Liu <kennyliu@deadgho.st>
2015-01-10 03:07:10 +0700

    Solved ex-003.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject project-euler "0.1.0-SNAPSHOT"
+  :description "Solving project euler in clojure."
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :profiles {}
+  :local-repo-classpath true)
---
+++ src/project_euler/ex_003.clj
@@ -0,0 +1,45 @@
+(ns project-euler.ex-003
+  (:require
+   [clojure.math.numeric-tower :as math]))
+
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;; What is the largest prime factor of the number 600851475143 ?
+
+;; TODO Cache prime numbers in a lazy infinite seq.
+
+(defn find-largest-factor
+  "Find largest factor of n."
+  [n]
+  (first (for [divisor (iterate dec (math/floor (/ n 2)))
+               :when (or (= divisor 0)
+                         (= divisor 1)
+                         (zero? (mod n divisor)))]
+           (if (or (= divisor 1)
+                   (= divisor 0))
+             n
+             divisor))))
+
+(defn find-smallest-factor
+  "Find smallest factor of n."
+  [n]
+  (first (for [divisor (iterate inc 2)
+               :when (or (zero? (mod n divisor))
+                         (> divisor (math/floor (/ n 2))))]
+           (if (> divisor (math/floor (/ n 2)))
+             n
+             divisor))))
+
+(defn is-prime?
+  [n]
+  (if (= (find-smallest-factor n) n)
+    n
+    nil))
+
+(defn find-largest-prime-factor [n]
+  (first (for [i (iterate dec (math/floor #_(/ n 2) (math/sqrt n)))
+               :when (and (== (mod n i) 0)
+                          (or (is-prime? i)
+                              (== i 1)))]
+           (if (== i 1)
+             n
+             i))))
