01dd506dd28ab041c7341728fc9fb1e9b702310d
Kenny Liu <kennyliu@deadgho.st>
2015-01-21 09:40:53 +0700

    Factored out take-until into util.clj.

---
+++ src/project_euler/util.clj
@@ -0,0 +1,12 @@
+(ns project-euler.util)
+
+(defn take-until
+  "Returns a lazy sequence of successive items from coll until
+  (pred item) returns true, including that item. pred must be
+  free of side-effects."
+  [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (if (pred (first s))
+       (cons (first s) nil)
+       (cons (first s) (take-until pred (rest s)))))))
