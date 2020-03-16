2f95b4ccb301c80a0381473b56c99d46511e2ef0
deadghost <kennyoliu@gmail.com>
2020-03-16 00:44:26 -0400

    math.combinatorics 0.0.8 -> 0.1.6

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Solving project euler in clojure."
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/algo.generic "0.1.3"]
                  [net.mikera/core.matrix "0.32.1"]]
   :profiles {}
